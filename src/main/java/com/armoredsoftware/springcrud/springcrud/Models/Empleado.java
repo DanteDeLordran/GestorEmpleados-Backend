f4ab514c7bb2786e567f6d4693398f66ea02b32c
Dante de Lordran <dantelopez305@outlook.com>
2023-01-13 19:51:07 -0600

    Initialized project

---
+++ src/main/java/com/armoredsoftware/springcrud/springcrud/Models/Empleado.java
@@ -0,0 +1,10 @@
+package com.armoredsoftware.springcrud.springcrud.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "empleados")
+public class Empleado {
+    
+}
