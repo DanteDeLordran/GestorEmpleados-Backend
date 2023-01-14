59b999236f630160dec07b7e2278f27df4ad572f
Dante de Lordran <dantelopez305@outlook.com>
2023-01-13 21:17:01 -0600

    Finished Empleado model

--- src/main/java/com/armoredsoftware/springcrud/springcrud/Models/Empleado.java
+++ src/main/java/com/armoredsoftware/springcrud/springcrud/Models/Empleado.java
@@ -1,10 +1,36 @@
 package com.armoredsoftware.springcrud.springcrud.Models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "empleados")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Empleado {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false, length = 25 )
+    private String nombre;
+
+    @Column(nullable = false , length = 25)
+    private String primer_apellido;
+
+    @Column(nullable = false , length = 30, unique = true)
+    private String email;
+
 }
